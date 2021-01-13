a3291ae112f70d6a996419b01f3fc6c63211bb77
Erwin Matijsen <erwin@academie.dev>
2021-01-13 21:38:30 +0100

    Eerste Model en migraties toevoegen

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    publish_date = models.DateField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
