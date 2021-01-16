480ef0bbb8052b86bd54686ff2a7f4e0518d4d86
Erwin Matijsen <erwin@academie.dev>
2021-01-16 15:03:32 +0100

    Toevoegen Category Model

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,16 @@ class Post(models.Model):
     publish_date = models.DateField(blank=True, null=True)
     published = models.BooleanField(default=False)
     author = models.CharField(max_length=200)
+    category = models.ForeignKey("blog.Category",
+                                 on_delete=models.PROTECT,
+                                 related_name="posts",
+                                 null=True,
+                                 blank=True,
+                                 )
+
+    class Meta:
+        verbose_name = "Bericht"
+        verbose_name_plural = "Berichten"
 
     def save(self, *args, **kwargs):
 
@@ -18,4 +28,16 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.id})"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Categorie"
+        verbose_name_plural = "Categorieën"
+
+    def __str__(self):
+        return f"{self.title} ({self.posts.count()})"
