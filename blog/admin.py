2f5aa439155cd755dbfc237f1531435a5106dd9d
Erwin Matijsen <erwin@academie.dev>
2021-01-16 14:04:51 +0100

    Updaten Admin omgeving

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from blog.models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "publish_date"
+    list_display = ("title", "publish_date", "published", "author")
+    list_editable = ("published", )
+    list_filter = ("published", )
+    search_fields = ("title", )
+
+
+admin.site.site_header = admin.site.site_title = "Beginnen met Django Blog"
+admin.site.index_title = "Administratie"
